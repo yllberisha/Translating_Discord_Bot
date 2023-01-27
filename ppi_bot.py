5a94bcd4b72eed351b7aa66e0b618e14f588885a
yllberisha <102769384+yllberisha@users.noreply.github.com>
2023-01-27 21:50:11 +0100

    Version 1.0
    The first Version of Translating Discord Bot

---
+++ ppi_bot.py
@@ -0,0 +1,68 @@
+import discord
+from discord.ext import tasks, commands
+from discord.utils import get
+from dotenv import load_dotenv
+import asyncio
+import os
+import googletrans 
+from discord import Embed
+
+translator = googletrans.Translator()
+
+# Create a dictionary of flag emojis and their corresponding language codes
+flag_emoji_dict = {
+"🇺🇸": "en",
+"🇩🇪": "de",
+"🇫🇷": "fr",
+"🇪🇸": "es",
+"🇮🇹": "it",
+"🇵🇹": "pt",
+"🇷🇺": "ru",
+"🇦🇱": "sq",
+"🇸🇦": "ar",
+"🇧🇦": "bs",
+"🇨🇳": "zh-CN",
+"🇹🇷": "tr",
+"🇵🇱": "pl",
+"🇳🇴": "no",
+"🇸🇬": "sv",
+"🇯🇵": "ja",
+"🇰🇷": "ko",
+}
+
+#For a more secure, we loaded the .env file and assign the token value to a variable 
+load_dotenv(".env")
+TOKEN = os.getenv("TOKEN")
+
+#Intents are permissions for the bot that are enabled based on the features necessary to run the bot.
+intents=discord.Intents.all()
+
+#Comman prefix is setup here, this is what you have to type to issue a command to the bot
+prefix = './'
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+
+#------------------------------------------------Events------------------------------------------------------#
+
+@bot.event
+async def on_reaction_add(reaction, user):
+  
+    # Check if the reaction is a flag emoji
+    if reaction.emoji in flag_emoji_dict:
+        # Get the language code corresponding to the flag emoji
+        lang_code = flag_emoji_dict[reaction.emoji]
+        # Get the original message
+        message = reaction.message
+        # Translate the message to the desired language
+        detected_lang = translator.detect(message.content)
+        translated_message = translator.translate(message.content, dest=lang_code).text
+        pronunciation_message =translator.translate(message.content, dest=lang_code).pronunciation
+
+        embed = Embed(title='Translated Text', description=f'{translated_message}', color=0x00ff00)
+        embed.add_field(name="Original Text", value=message.content, inline=False)
+        embed.add_field(name="Translated from:", value=f'{detected_lang.lang.capitalize()} ({detected_lang.confidence*100:.2f}%)')
+        embed.add_field(name="Pronunciation:", value=pronunciation_message, inline=False)
+       # await reaction.message.channel.send(content=f'{reaction.user.mention}',embed=embed)
+        await reaction.message.channel.send(content=f'{user.mention}',embed=embed)
+
+#Run the bot
+bot.run(TOKEN)
